d1a7349aca531a031d90e1c4a2fa46142e6333ab
gayat <gayathrikasi@gmail.com>
2024-03-06 20:38:45 +0100

    Added RemoveiF and get Methods

--- src/samplepakage/ListMethods.java
+++ src/samplepakage/ListMethods.java
@@ -1,2 +1,17 @@
-package samplepakage;public class ListMethods {
+package samplepakage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListMethods {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(2);
+        System.out.println("list = " + list);
+        System.out.println("list.get(0)); = " + list.get(0));
+        System.out.println("list.removeIf() = "
+                + list.removeIf(p -> p % 2 != 0));
+    }
 }
